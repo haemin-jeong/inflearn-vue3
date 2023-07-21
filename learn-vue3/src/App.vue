da78b1edceaaca5e558f2ae56c705d292b00e1e8
jeonghaemin <jhm0828@gmail.com>
2023-07-22 00:01:18 +0900

    setup 함수

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,11 +1,26 @@
 <template>
-	<div></div>
+	<div>
+		<p>{{ counter }}</p>
+		<p>{{ message }}</p>
+		<button @click="increment">click!</button>
+	</div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const counter = ref(0);
+		const message = ref('hello Vue3');
+		const increment = () => {
+			counter.value++;
+		};
+		return {
+			counter,
+			message,
+			increment,
+		};
 	},
 };
 </script>
