a98d67be162441156b2528db3b3f1f8995a117d4
jeonghaemin <jhm0828@gmail.com>
2023-07-21 23:35:40 +0900

    Composition API

---
+++ learn-vue3/src/lectures/4/App_compositionAPI.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<h2>반응형 메시지</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button v-on:click="addReactiveMessage">Add message</button>
+		<h2>일반 메시지</h2>
+		<p>{{ normalMessage }}</p>
+		<button v-on:click="addNormalMessage">Add message</button>
+	</div>
+</template>
+
+<script>
+import { isRef, onBeforeMount, onMounted, ref } from 'vue';
+export default {
+	setup() {
+		console.log('setup()');
+		const reactiveMessage = ref('hello reactive message');
+		const addReactiveMessage = () => {
+			reactiveMessage.value = reactiveMessage.value + '!';
+		};
+		console.log('isRef(reactiveMessage): ', isRef(reactiveMessage));
+		const normalMessage = 'hello normal message';
+		const addNormalMessage = () => {
+			normalMessage.value = normalMessage.value + '!';
+		};
+		console.log('isRef(normalMessage): ', isRef(normalMessage));
+
+		onMounted(() => {
+			console.log('onMounted');
+		});
+		onBeforeMount(() => {
+			console.log('onBeforeMounted');
+		});
+		return {
+			reactiveMessage,
+			normalMessage,
+			addReactiveMessage,
+			addNormalMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
