9931aa2ca217376f358e1a525ed6d4f633ad67c8
jeonghaemin <jhm0828@gmail.com>
2023-07-22 15:10:08 +0900

    setup함수, 템플릿 문법, 반응형 기초(Reactivity)

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,26 +1,11 @@
 <template>
-	<div>
-		<p>{{ counter }}</p>
-		<p>{{ message }}</p>
-		<button @click="increment">click!</button>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { ref } from 'vue';
-
 export default {
 	setup() {
-		const counter = ref(0);
-		const message = ref('hello Vue3');
-		const increment = () => {
-			counter.value++;
-		};
-		return {
-			counter,
-			message,
-			increment,
-		};
+		return {};
 	},
 };
 </script>
