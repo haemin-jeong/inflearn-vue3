9997d7d0787f92953d56d43d4b56561dda99599f
jeonghaemin <jhm0828@gmail.com>
2023-07-23 23:19:02 +0900

    computed ~ 이벤트 처리

---
+++ learn-vue3/src/lectures/9/App_class_binding.vue
@@ -0,0 +1,42 @@
+<template>
+	<div :class="[isActive ? 'active-class' : 'class', errorClass, classObject]">텍스트 입니다.</div>
+	<button v-on:click="toggle">toggle</button>
+	<button v-on:click="hasError = !hasError">toggle erorr</button>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+		// const classObject = reactive({
+		// 	active: true,
+		// 	'text-dager': false,
+		// })
+
+		const classObject = computed(() => {
+			return {
+				active: true && true,
+				'text=danger': true && true,
+			};
+		});
+
+		const activeClass = ref('active');
+		const errorClass = ref('error');
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+
+		return { isActive, toggle, hasError, classObject, activeClass, errorClass };
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+}
+</style>
---
+++ learn-vue3/src/lectures/9/App_style_binding.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, veritatis
+			impedit et aut culpa omnis dolores accusamus iure dolore mollitia, maiores
+			reiciendis dolorum perspiciatis sapiente, minima consectetur. Ut, alias
+			temporibus.
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fondSize: '13px',
+		// });
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return { styleObject, fontSize };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
